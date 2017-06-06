ea6384b6b62f313f2e672f71cc101018ef852aa8
awalqui <walqang@gmail.com>
2017-06-06 10:55:51 -0400

    Add a name to the details

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const personForm = document.querySelector('form#personForm')
 
 function handleSubmit(ev) {
-    ev.preventDefault()
-    const f = ev.target
-    const name = f.personName.value
-    const age = f.personAge.value
-    const colour = f.favColor.value
-    personForm.style.background = colour
-    document.querySelector('h1').textContent = name + " " + age
+  ev.preventDefault()
+  const f = ev.target
+  const details = document.querySelector('#details')
+  const name = f.personName.value
+  
+  details.innerHTML = '<em>' + name + '</em>'
 }
 
 personForm.addEventListener('submit', handleSubmit)
